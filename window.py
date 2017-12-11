167614719364152744864e58cb64200670554bef
neketka <neketka@gmail.com>
2017-12-10 22:07:35 -0500

    Added drawing module.

--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 from OpenGL.GLUT import *
 from vbo import *
 from shader import *
+from drawing import *
 import numpy as np
 import glmath
 import sys
@@ -15,6 +16,7 @@ class Window:
         glutIdleFunc(self.__logic)
         glutCloseFunc(self.__destroy)
         glutDisplayFunc(self.__render)
+        glutReshapeFunc(self.__resize)
         self.shader = Shader(ShaderStage(ShaderStageType.VertexShader, "./shaders/test.vert"),
                              ShaderStage(ShaderStageType.FragmentShader, "./shaders/test.frag"))
 
@@ -28,18 +30,19 @@ class Window:
             0, 1, 2
         ]))
 
-        self.binding = self.vbo.createBinding([AttribBinding(False, self.shader.getAttribLocation("pos"), 4, 2)])
+        self.drawing = DrawingOperation(self.shader, [],
+            [self.vbo.createBinding([AttribBinding(False, self.shader.getAttribLocation("pos"), 4, 3)])], self.ibo)
 
     def __logic(self):
         pass
 
     def __render(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        self.shader.use()
-        self.ibo.bind()
-        self.binding.bindData()
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0)
+        self.drawing.draw()
         glutSwapBuffers()
+
+    def __resize(self, w, h):
+        pass
 
     def __destroy(self):
         self.shader.delete()
